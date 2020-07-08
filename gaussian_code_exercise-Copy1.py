40eaa6a20d747d71999c8f35d8fed2a00afa979a
Sumeet Kumar Sahoo <62834314+sumu007@users.noreply.github.com>
2020-07-08 17:08:47 +0530

    Add files via upload

---
+++ gaussian_code_exercise-Copy1.py
@@ -0,0 +1,162 @@
+
+import math
+import matplotlib.pyplot as plt
+
+class Gaussian():
+    """ Gaussian distribution class for calculating and 
+    visualizing a Gaussian distribution."""
+    def __init__(self, mu = 0, sigma = 1):
+        
+        self.mean = mu
+        self.stdev = sigma
+        self.data = []
+
+    
+    def calculate_mean(self):
+    
+        """Function to calculate the mean of the data set."""
+                    
+        avg = 1.0 * sum(self.data) / len(self.data)
+        
+        self.mean = avg
+        
+        return self.mean
+
+
+
+    def calculate_stdev(self, sample=True):
+
+        """Function to calculate the standard deviation of the data set."""
+
+        if sample:
+            n = len(self.data) - 1
+        else:
+            n = len(self.data)
+    
+        mean = self.mean
+    
+        sigma = 0
+    
+        for d in self.data:
+            sigma += (d - mean) ** 2
+        
+        sigma = math.sqrt(sigma / n)
+    
+        self.stdev = sigma
+        
+        return self.stdev
+        
+
+    def read_data_file(self, file_name, sample=True):
+    
+        """Function to read in data from a txt file. The txt file should have
+        one number (float) per line. The numbers are stored in the data attribute. 
+        After reading in the file, the mean and standard deviation are calculated"""
+            
+        with open(file_name) as file:
+            data_list = []
+            line = file.readline()
+            while line:
+                data_list.append(int(line))
+                line = file.readline()
+        file.close()
+    
+        self.data = data_list
+        self.mean = self.calculate_mean()
+        self.stdev = self.calculate_stdev(sample)
+        
+        
+    def plot_histogram(self):
+        """Function to output a histogram of the instance variable data using 
+        matplotlib pyplot library."""
+        plt.hist(self.data)
+        plt.title('Histogram of Data')
+        plt.xlabel('data')
+        plt.ylabel('count')
+        
+        
+        
+    def pdf(self, x):
+        """Probability density function calculator for the gaussian distribution."""
+        
+        return (1.0 / (self.stdev * math.sqrt(2*math.pi))) * math.exp(-0.5*((x - self.mean) / self.stdev) ** 2)
+        
+
+    def plot_histogram_pdf(self, n_spaces = 50):
+
+        """Function to plot the normalized histogram of the data and a plot of the 
+        probability density function along the same range"""
+        
+        mu = self.mean
+        sigma = self.stdev
+
+        min_range = min(self.data)
+        max_range = max(self.data)
+        
+         # calculates the interval between x values
+        interval = 1.0 * (max_range - min_range) / n_spaces
+
+        x = []
+        y = []
+        
+        # calculate the x values to visualize
+        for i in range(n_spaces):
+            tmp = min_range + interval*i
+            x.append(tmp)
+            y.append(self.pdf(tmp))
+
+        # make the plots
+        fig, axes = plt.subplots(2,sharex=True)
+        fig.subplots_adjust(hspace=.5)
+        axes[0].hist(self.data, density=True)
+        axes[0].set_title('Normed Histogram of Data')
+        axes[0].set_ylabel('Density')
+
+        axes[1].plot(x, y)
+        axes[1].set_title('Normal Distribution for \n Sample Mean and Sample Standard Deviation')
+        axes[0].set_ylabel('Density')
+        plt.show()
+
+        return x, y
+
+
+# In[2]:
+
+
+# Unit tests to check your solution
+
+import unittest
+
+class TestGaussianClass(unittest.TestCase):
+    def setUp(self):
+        self.gaussian = Gaussian(25, 2)
+
+    def test_initialization(self): 
+        self.assertEqual(self.gaussian.mean, 25, 'incorrect mean')
+        self.assertEqual(self.gaussian.stdev, 2, 'incorrect standard deviation')
+
+    def test_pdf(self):
+        self.assertEqual(round(self.gaussian.pdf(25), 5), 0.19947,         'pdf function does not give expected result') 
+
+    def test_meancalculation(self):
+        self.gaussian.read_data_file('numbers.txt', True)
+        self.assertEqual(self.gaussian.calculate_mean(),         sum(self.gaussian.data) / float(len(self.gaussian.data)), 'calculated mean not as expected')
+
+    def test_stdevcalculation(self):
+        self.gaussian.read_data_file('numbers.txt', True)
+        self.assertEqual(round(self.gaussian.stdev, 2), 92.87, 'sample standard deviation incorrect')
+        self.gaussian.read_data_file('numbers.txt', False)
+        self.assertEqual(round(self.gaussian.stdev, 2), 88.55, 'population standard deviation incorrect')
+                
+tests = TestGaussianClass()
+
+tests_loaded = unittest.TestLoader().loadTestsFromModule(tests)
+
+unittest.TextTestRunner().run(tests_loaded)
+
+
+# In[ ]:
+
+
+
+
